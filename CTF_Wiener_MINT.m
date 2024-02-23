55d75d07c797bf410e6f52f8bceea3849325ab15
anchi_lab <a0926129380@gmail.com>
2024-02-23 15:37:04 +0800

    刪除 Wiener中檢查impulse的部分 刪除313.wav

--- CTF_Wiener_MINT.m
+++ CTF_Wiener_MINT.m
@@ -275,22 +275,6 @@ for i = 1 : MicNum
     A_forplot(:, :, i) = squeeze(A(i, :, :)).';
 end
 
-% ATF_frame = (points_rir - NFFT)/hopsize + 1 + (round(NFFT/hopsize)-1);    % 使 ATF 長度等於 points_rir + (osfac-1)*hopsize (因為回時域後還許把前 (osfac-1)*hopsize 點去掉)
-% impulse = zeros(1, ((ATF_frame+L-1)-1)*hopsize+NFFT);    % impulse signal 在頻域的的長度需等於 ATF_frame + L - 1 (因為 ?-L+1=ATF_frame ?=ATF_frame+L-1)
-% impulse(:, (L-1)*hopsize + 1) = 1;    % 因為是 convolution 所以 impulse 必須在 (L-1)*hopsize + 1 等等 flip 來才會是第一個 frame
-% impulse_transpose = impulse.';
-% [IMPULSE, ~, ~] = stft(impulse_transpose, fs, Window=win, OverlapLength=NFFT-hopsize, FFTLength=NFFT, FrequencyRange='onesided');
-% 
-% A_dB = mag2db(abs(IMPULSE));
-% % 畫 A frequency plot %
-% figure(6);
-% mesh(1:1:102, 1:1:513, A_dB)
-% colorbar
-% xlabel('frame')
-% ylabel('frequency(Hz)')
-% zlabel('dB')
-% shg
-
 A_tdomain = reconstruct_RIR_normalwindow(points_rir, NFFT, hopsize, L, win, fs, frequency, MicNum, A_forplot);    % dimension = MicNum x (points_rir+(osfac-1)*hopsize)
 ratio_A_tdomain = zeros(MicNum, 1);
 for i = 1:MicNum
