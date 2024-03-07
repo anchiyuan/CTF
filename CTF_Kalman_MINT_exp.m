a27d718524a627af6b62beb12d200dfb87eb0674
anchi_lab <a0926129380@gmail.com>
2024-03-07 15:49:32 +0800

    新增meeting.pdf wpe.mat檔多底線區分第幾顆喇叭 新增BSI的exp但是跑了之後會記憶體不足因為rir點數太多

--- CTF_Kalman_MINT_exp.m
+++ CTF_Kalman_MINT_exp.m
@@ -46,7 +46,8 @@ y_nodelay = zeros(MicNum, SorLen);
 for i = 1:MicNum
     y_nodelay_str = ['wav_exp\', string(i-1), 'th.wav'];
     y_nodelay_filename = join(y_nodelay_str, '');
-    [y_nodelay(i, :), fs] = audioread( y_nodelay_filename, [1, SorLen]);    % 第三顆喇叭需換 duration
+%     [y_nodelay(i, :), fs] = audioread( y_nodelay_filename, [1, SorLen]);    % 第一顆喇叭
+    [y_nodelay(i, :), fs] = audioread( y_nodelay_filename, [30*fs+1, 30*fs+SorLen]);    % 第三顆喇叭
 end
 
 % load y %
@@ -65,18 +66,15 @@ y_delay_transpose = y_delay.';
 % y_wpe = y_wpe.';
 % 
 % % 存 wpe mat %
-% save('y_exp\y_wpe.mat', 'y_wpe')
-% 
-% % 存 wpe wav %
-% ratio_y_wpe = 0.8 / max(abs(y_wpe(look_mic, :))) ;
-% audiowrite('wav_exp\y_wpe_all.wav', y_wpe(look_mic, :)*ratio_y_wpe, fs)
+% save('y_exp\y_wpe_3.mat', 'y_wpe')
 
 % load y_wpe %
-load('y_exp\y_wpe.mat');
+load('y_exp\y_wpe_3.mat');
 
 %% DAS beamformer (Y_wpe and Y_DAS) %%
 % 算 mic 與 source 之距離 %
-distance = [1.1; 1.153; 1.203; 1.253; 1.303; 1.36];    % [0.832; 0.83; 0.834; 0.84; 0.852; 0.87];
+% distance = [1.1; 1.153; 1.203; 1.253; 1.303; 1.36];    % 第一顆喇叭
+distance = [0.832; 0.83; 0.834; 0.84; 0.852; 0.87];    % 第三顆喇叭
 
 % 算 a %
 a = zeros(MicNum, SorNum, frequency);
@@ -104,8 +102,9 @@ end
 start_ini_frame = L;
 ini_frame = NumOfFrame;
 
-% Kalman filter %
 A = zeros(MicNum, L, frequency);
+
+% Kalman stationary filter %
 tic
 parfor i = 1:MicNum
     for n =1:frequency
@@ -125,6 +124,32 @@ parfor i = 1:MicNum
 
 end
 toc
+
+% Kalman nonstationary filter %
+% tic
+% parfor i = 1:MicNum
+%     for n =1:frequency
+%         weight = zeros(L, 1);
+%         P = 0.5*eye(L);    % error covariance matrix %
+%         K = zeros(L, 1);    % Kalman gain %
+%         Q = 10^(-4)*eye(L);    % process noise covariance matrix %
+%         R = 10^(-3);    % measurement noise covariance matrix %
+%         for FrameNo = start_ini_frame:ini_frame
+%             % time update %
+%             P = P + Q;
+% 
+%             % measurement update %
+%             K = P*flip(Y_DAS(n, FrameNo-L+1:FrameNo).')*inv(conj(flip(Y_DAS(n, FrameNo-L+1:FrameNo)))*P*flip(Y_DAS(n, FrameNo-L+1:FrameNo).') + R);
+%             weight = weight + K*(conj(Y_delay(n, FrameNo, i)) - conj(flip(Y_DAS(n, FrameNo-L+1:FrameNo)))*weight);
+%             P = P - K*conj(flip(Y_DAS(n, FrameNo-L+1:FrameNo)))*P;
+% 
+%         end
+% 
+%         A(i, :, n) = weight';
+%     end
+% 
+% end
+% toc
 
 A_dB = mag2db(abs(A));
 % 畫 A frequency plot %
@@ -157,6 +182,7 @@ for i = 1:MicNum
     t60(i, :) = t60_point/fs;
 end
 
+look_mic = 1;
 figure(3)
 plot(tf(look_mic, :));
 title('tfestimate')
@@ -183,9 +209,26 @@ A_tdomain_forplot = A_tdomain(look_mic, hopsize*(osfac-1)+1:end);
 figure(4)
 plot(tf(look_mic, :), 'r');
 hold on
-plot(-A_tdomain_forplot, 'b');
+plot(A_tdomain_forplot, 'b');
 hold off
 legend('MTF', 'CTF')
 xlabel('points')
 ylabel('amplitude')
 shg
+
+%% evaluation metrics %%
+% NRMSPM %
+h_NRMSPM = reshape(tf.', [MicNum*points_rir 1]);
+aa_NRMSPM = reshape(A_tdomain(:, hopsize*(osfac-1)+1:end).', [MicNum*points_rir 1]);
+NRMSPM = 20*log(norm(h_NRMSPM-h_NRMSPM.'*aa_NRMSPM/(aa_NRMSPM.'*aa_NRMSPM)*aa_NRMSPM)/norm(h_NRMSPM));
+
+% ME %
+ATF = fft(tf, points_rir, 2);
+ATF_estimated = fft(A_tdomain(:, hopsize*(osfac-1)+1:end), points_rir, 2);
+sum_norm = 0;
+for i  = 1:MicNum
+    norm_ATF = norm(ATF(i, :) - ATF_estimated(i, :));
+    sum_norm = sum_norm + norm_ATF;
+end
+
+ME = sum_norm/MicNum;
